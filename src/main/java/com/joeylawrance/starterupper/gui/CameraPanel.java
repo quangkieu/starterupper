deccefa9c18585f49697ede0b9175fe4a6396e89
Joey Lawrance <lawrancej@wit.edu>
2014-01-28 01:22:57 -0500

    Fix for issue #11: Hard crash when no webcam is found.

--- src/main/java/com/joeylawrance/starterupper/gui/CameraPanel.java
+++ src/main/java/com/joeylawrance/starterupper/gui/CameraPanel.java
@@ -1,62 +1,141 @@
 package com.joeylawrance.starterupper.gui;
 
+import java.awt.CardLayout;
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
 
 @SuppressWarnings("serial")
-public class CameraPanel extends WebcamPanel {
+public class CameraPanel extends JPanel {
+	private static final Logger logger = LoggerFactory.getLogger(CameraPanel.class);
 	private Webcam cam;
+	private WebcamPanel panel;
+	private final JLabel snapshot;
+	JPanel empty;
 	private File image;
+	private boolean started = false;
+	private CardLayout layout;
 	public CameraPanel(File image) {
 		this(Webcam.getDefault(), image);
 	}
 	public CameraPanel(Webcam webcam, File image) {
-		super(webcam, new Dimension(240,240), false);
-		cam = webcam;
+		// There's three states to this panel: the cam, snapshot, or empty (no cam or image).
+		layout = new CardLayout(0,0);
+		setLayout(layout);
+
+		// Default card in case the user has no cam or gravatar.
+		empty = new JPanel();
+		empty.setName("empty");
+		add(empty);
+		layout.show(this, empty.getName());
+
+		// Only create the webcam panel if we've got a webcam.
+		if (webcam != null) {
+			panel = new WebcamPanel(webcam, new Dimension(320,240), false);
+			panel.setName("cam");
+			cam = webcam;
+			add(panel, panel.getName());
+			layout.show(this, panel.getName());
+		}
+
+		// Create a snapshot regardless of whether we got one.
 		this.image = image;
+		snapshot = new JLabel();
+		snapshot.setName("snapshot");
+		add(snapshot, snapshot.getName());
+
+		// But only show an image if it already exists
+		if (image.exists()) {
+			snapshot.setIcon(new ImageIcon(image.getAbsolutePath()));
+			layout.show(this, snapshot.getName());
+		}
+		// Don't start the cam until we're actually visible
 		this.addAncestorListener(new AncestorListener() {
 			@Override
 			public void ancestorAdded(AncestorEvent arg0) {
-				if (!CameraPanel.this.image.exists()) {
-					CameraPanel.this.start();
-				}
+				start();
 			}
 			@Override
 			public void ancestorMoved(AncestorEvent arg0) {
 			}
 			@Override
 			public void ancestorRemoved(AncestorEvent arg0) {
-				CameraPanel.this.stop();
+				stop(true);
 			}
 		});
 	}
-	public void take() throws Exception {
-		BufferedImage bi = cam.getImage();
-		ImageIO.write(bi, "jpg", image);
-		this.stop();
-	}
-	public void discard() throws Exception {
-		image.delete();
-		this.start();
-	}
-	public void paintComponent(Graphics g) {
-		if (image.exists()) {
-			try {
-				g.drawImage(ImageIO.read(image), 0, 0, null);
-			} catch (IOException e) {
-			}
-		} else {
-			super.paintComponent(g);
+	/**
+	 * Start the camera.
+	 */
+	public void start() {
+		if (!image.exists() && panel != null) {
+			layout.show(this, panel.getName());
+			if (started) {
+				panel.resume();
+			} else {
+				panel.start();
+				started = true;
+			} 
 		}
+	}
+	/**
+	 * Stop the camera.
+	 * @param forReal if true, stop; otherwise, pause.
+	 */
+	public void stop(boolean forReal) {
+		if (panel != null && started) {
+			if (!forReal) {
+				panel.pause();
+			} else {
+				started = false;
+				panel.stop();
+			}
+		}
+	}
+	/**
+	 * Can we take a picture?
+	 * @return
+	 */
+	public boolean canTakePicture() {
+		return started;
+	}
+	/**
+	 * Take a picture
+	 * @throws Exception
+	 */
+	public void take() {
+		if (cam != null) {
+			try {
+				BufferedImage bi = cam.getImage();
+				ImageIO.write(bi, "jpg", image);
+				snapshot.setIcon(new ImageIcon(bi));
+				layout.show(this, snapshot.getName());
+				stop(false);
+			} catch (Exception e) {
+				logger.error("Unable to save image. {}", e.toString());
+				layout.show(this, empty.getName());
+			}
+		}
+	}
+	public void discard() {
+		try {
+			image.delete();
+		} catch (Exception e) {
+			logger.error("Unable to delete image. {}", e.toString());
+		}
+		start();
 	}
 }
--- src/main/java/com/joeylawrance/starterupper/gui/PicturePanel.java
+++ src/main/java/com/joeylawrance/starterupper/gui/PicturePanel.java
@@ -7,6 +7,10 @@ import javax.swing.JPanel;
 
 import net.miginfocom.swing.MigLayout;
 
+import com.google.common.eventbus.Subscribe;
+import com.joeylawrance.starterupper.model.Event;
+import com.joeylawrance.starterupper.model.host.HostAction;
+import com.joeylawrance.starterupper.model.host.HostPerformedAction;
 import com.joeylawrance.starterupper.model.host.impl.Gravatar;
 
 import javax.swing.JButton;
@@ -19,66 +23,65 @@ public class PicturePanel extends JPanel {
 	final JButton sharePicture;
 	final JButton takePicture;
 	final JButton discardPicture;
+	final Gravatar model;
 	CameraPanel camPanel;
 	public PicturePanel(Gravatar model) {
+		this.model = model;
 		setLayout(new MigLayout("", "[320.00,grow]", "[][240.00][grow][]"));
 		setName("Profile picture");
-		
+
 		add(new JLabel("Smile! Take a picture and share it to associate names and faces."), "cell 0 0,alignx center");
 
 		camPanel = new CameraPanel(model.getProfilePicture());
 		add(camPanel, "cell 0 1,alignx center");
-		
+
 		JPanel panel = new JPanel();
 		add(panel, "cell 0 2,alignx center,growy");
 		panel.setLayout(new MigLayout("", "[][][]", "[]"));
-		
+
 		takePicture = new JButton("Take");
-		takePicture.setToolTipText("Take a picture.");
+		takePicture.setToolTipText("Take a picture");
 		takePicture.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				try {
-					camPanel.take();
-					sharePicture.setEnabled(true);
-					discardPicture.setEnabled(true);
-					takePicture.setEnabled(false);
-				} catch (Exception e) {
-					// set error
-				}
+				camPanel.take();
+				sharePicture.setEnabled(true);
+				discardPicture.setEnabled(true);
+				takePicture.setEnabled(false);
 			}
-			
+
 		});
-		
+
 		panel.add(takePicture, "cell 0 0");
-		
+
 		discardPicture = new JButton("Discard");
-		discardPicture.setToolTipText("Discard picture.");
+		discardPicture.setToolTipText("Discard picture");
 		discardPicture.setEnabled(model.getProfilePicture().exists());
 		panel.add(discardPicture, "cell 1 0");
 		discardPicture.addActionListener(new ActionListener() {
-
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				try {
-					camPanel.discard();
-					sharePicture.setEnabled(false);
-					discardPicture.setEnabled(false);
-					takePicture.setEnabled(true);
-				} catch (Exception e) {
-					// set error
-				}
+				camPanel.discard();
+				sharePicture.setEnabled(false);
+				discardPicture.setEnabled(false);
+				takePicture.setEnabled(camPanel.canTakePicture());
 			}
-			
 		});
-		
+
 		sharePicture = new JButton("Share");
-		sharePicture.setToolTipText("Share picture via Gravatar.");
-		sharePicture.setEnabled(model.getProfilePicture().exists());
+		sharePicture.setToolTipText("Share picture via Gravatar");
+		sharePicture.setEnabled(false);
 		panel.add(sharePicture, "cell 2 0");
-		
+
 		JLabel error = new JLabel("");
 		error.setForeground(Color.RED);
 		add(error, "cell 0 3,alignx center");
+		Event.getBus().register(this);
+	}
+	@Subscribe
+	public void gravatarLoggedIn(HostPerformedAction event) {
+		if (event.host.equals(model) && event.action == HostAction.login) {
+			sharePicture.setEnabled(model.getProfilePicture().exists());
+		}
 	}
 }
