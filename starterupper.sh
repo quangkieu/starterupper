0c36684304d683f1d0401b7c5511e93efe4e537b
Joey Lawrance <lawrancej@wit.edu>
2014-05-15 15:05:05 -0400

    Numerous validation improvements, firewall bypassing.

--- starterupper.sh
+++ starterupper.sh
@@ -21,13 +21,30 @@ SCHOOL=wit.edu
 # add email to github via api, if not set (e.g., for existing users registered with a different email)
 # ask user to validate email if not validated
 # check if email is validated via api on github before attempting to create a private repo
-# check network connectivity
-# determine if username is found via api as a last minute sanity check
 # go through all pages when fetching usernames
 # grading interface: checkout stuff rapid fire like, possibly use git notes
+# fall back to https remotes if the school doesn't support SSH
+# if the public key already exists on another account, ask user if they'd like to wipe existing keypair and generate a new one.
+
+# Runtime flags (DO NOT CHANGE)
+# ---------------------------------------------------------------------
+ssh_works=true # unless proven otherwise...
+fullname=''
+email=''
+github_login=''
 
 # Utilities
 # ---------------------------------------------------------------------
+
+# Set $USERNAME, if not already set.
+get_username() {
+    if [[ -z "$USERNAME" ]]; then
+        USERNAME=$(id -nu 2> /dev/null)
+    fi
+    if [[ -z "$USERNAME" ]]; then
+        USERNAME=$(whoami 2> /dev/null)
+    fi
+}
 
 next_step() {
     read -p "Press enter to continue." < /dev/tty
@@ -44,8 +61,18 @@ file_open() {
     next_step
 }
 
-# Git functions
+# Local functions
 # ---------------------------------------------------------------------
+
+instructor_setup() {
+    user_is_instructor=true
+    git config --global user.name "$INSTRUCTOR_NAME"
+    fullname="$INSTRUCTOR_NAME"
+    git config --global user.email "$INSTRUCTOR_EMAIL"
+    email="$INSTRUCTOR_EMAIL"
+    git config --global login.github "$INSTRUCTOR_GITHUB"
+    github_login="$INSTRUCTOR_GITHUB"
+}
 
 # Ask the user if they are the instructor (by default, assume that they aren't)
 instructor_check() {
@@ -53,7 +80,7 @@ instructor_check() {
         while ! [[ -z "$value" ]] && [[ "$value" == "$instructor" ]]; do
             read -p "Are you the instructor (yes/no)? " user_is_instructor < /dev/tty
             case "$user_is_instructor" in
-                [Yy] | [Yy][Ee][Ss] ) user_is_instructor=true; return 0 ;;
+                [Yy] | [Yy][Ee][Ss] ) instructor_setup; return 0 ;;
                 [Nn] | [Nn][Oo] ) user_is_instructor=false; git config --global --unset $key; value=''; return 0 ;;
                 "" ) user_is_instructor=false; git config --global --unset $key; value=''; return 0 ;;
                 * ) echo "Please answer yes or no." ;;
@@ -62,8 +89,10 @@ instructor_check() {
     fi
 }
 
+# Check if the user is the instructor or complain if their value doesn't match the regex.
 validate_input() {
     instructor_check
+    
     if ! [[ -z "$value" ]] && [[ -z $(echo "$value" | grep -E "$validation_regex" ) ]]; then
         echo "ERROR: '$value' is not a $prompt. $invalid_prompt"
     fi
@@ -71,19 +100,23 @@ validate_input() {
 
 # Ask the user if they want to change their configuration in case of goof-ups (by default, assume that they don't)
 change_config() {
-    while ! [[ -z "$value" ]]; do
-        read -p "Is your $prompt $value (yes/no)? " yn < /dev/tty
-        case "$yn" in
-            [Yy] | [Yy][Ee][Ss] ) return 0 ;;
-            [Nn] | [Nn][Oo] ) git config --global --unset $key; value=''; return 0 ;;
-            "" ) return 0 ;;
-            * ) echo "Please answer yes or no." ;;
-        esac
-    done
+    # If we already set a value before, don't bother changing the config again
+    if [[ -z "$given_value" ]]; then
+        # Ask if the user is okay with what they set
+        while ! [[ -z "$value" ]]; do
+            read -p "Is your $prompt $value (yes/no)? " yn < /dev/tty
+            case "$yn" in
+                [Yy] | [Yy][Ee][Ss] ) return 0 ;;
+                [Nn] | [Nn][Oo] ) git config --global --unset $key; value=''; return 0 ;;
+                "" ) return 0 ;;
+                * ) echo "Please answer yes or no." ;;
+            esac
+        done
+    fi
 }
 
 # Ask user to set a key in ~/.gitconfig if it's not already set.
-# Sets $value
+# Sets $value only after the user's input passes $validation_regex
 set_key() {
     key=$1
     prompt=$2
@@ -91,6 +124,7 @@ set_key() {
     validation_regex=$4
     invalid_prompt=$5
     instructor=$6
+    given_value=$7
     
     value=$(git config --global $key)
 
@@ -103,12 +137,13 @@ set_key() {
     git config --global $key "$value"
 }
 
-# Sets $fullname and $email
-configure_git() {
-    set_key user.name "full name" "Jane Smith" "\w+ \w+" "Include your first and last name." "$INSTRUCTOR_NAME"
-    fullname=$value
-    set_key user.email "school email address" "smithj@$SCHOOL" "edu$" "Use your .edu address." "$INSTRUCTOR_EMAIL"
-    email=$value
+# Generate SSH public/private keypair, if it doesn't already exist.
+generate_ssh_keypair() {
+    if [[ ! -f ~/.ssh/id_rsa.pub ]]; then
+        echo "Generating SSH public/private keypair..."
+        # Use default location, set no phassphrase, no questions asked
+        printf "\n" | ssh-keygen -t rsa -N '' 2> /dev/null > /dev/null
+    fi
 }
 
 # Setup remotes for repository
@@ -127,48 +162,55 @@ configure_remotes() {
     cd ~
 }
 
-# SSH functions
-# ---------------------------------------------------------------------
-
-generate_key() {
-    if [[ ! -f ~/.ssh/id_rsa.pub ]]; then
-        printf "\n" | ssh-keygen -t rsa -N '' # Default location, no phassphrase, no questions asked
-    fi
-}
-
-copy_key_to_clipboard() {
-    case $OSTYPE in
-        msys | cygwin ) echo "Copied your public key to clipboard."; cat ~/.ssh/id_rsa.pub > /dev/clipboard ;;
-        linux* ) echo "Copied your public key to clipboard."; cat ~/.ssh/id_rsa.pub | xclip -selection clipboard ;; # -i ;;
-        darwin* ) echo "Copied your public key to clipboard."; cat ~/.ssh/id_rsa.pub | pbcopy ;;
-        * ) echo "Copy your public key (below) to the clipboard:"; cat ~/.ssh/id_rsa.pub ;;
-    esac
+# Setup git user.name, user.email and ssh keypair.
+# Sets: $fullname, $email, $USERNAME, $ssh_works
+local_setup() {
+    get_username
+    generate_ssh_keypair
+    set_key user.name "full name" "Jane Smith" "\w+ \w+" "Include your first and last name." "$INSTRUCTOR_NAME" "$fullname"
+    fullname="$value"
+    set_key user.email "school email address" "$USERNAME@$SCHOOL" "edu$" "Use your .edu address." "$INSTRUCTOR_EMAIL" "$email"
+    email="$value"
 }
 
 # Github functions
 # ---------------------------------------------------------------------
 
-github_join() {
-    if [ -z $(git config --global github.login) ]; then
+# Asks user to login to github after joining
+# Sets $github_login
+github_configure() {
+    local_setup
+    
+    if [ -z $(git config --global login.github) ]; then
         read -p "Do you have a Github account (yes or No [default])? " has_github_account < /dev/tty
         # Let's assume that they don't by default
         if [[ $has_github_account != [Yy]* ]]; then
             echo "Join Github. IMPORTANT: Use $email as your email address."
             sleep 2
             file_open "https://github.com/join"
-            echo "Open your school email inbox and verify your email with Github."
+            echo "Open your inbox and verify your email with Github."
             sleep 2
             next_step
         fi
     fi
+    if ! [[ -f ~/.token ]]; then
+        set_key login.github "Github username" "$USERNAME" "^[0-9a-zA-Z][0-9a-zA-Z-]*$" "See: https://github.com" "$INSTRUCTOR_GITHUB" "$github_login"
+        # determine if username is found via api as a last minute sanity check
+        while [[ -z $(github_user "$(git config --global login.github)" | grep "HTTP/... 20.") ]]; do
+            echo "ERROR: username $(git config --global login.github) does not exist on Github."
+            git config --global --unset login.github
+            set_key login.github "Github username" "$USERNAME" "^[0-9a-zA-Z][0-9a-zA-Z-]*$" "See: https://github.com" "$INSTRUCTOR_GITHUB" "$github_login"
+        done
+    fi
+    github_login=$(git config --global login.github)
 }
 
 # Wow, it's complicated
 # https://github.com/sessions/forgot_password
-# Sets $github_login and generates ~/.token with authentication token
+# Generates ~/.token with authentication token
 github_authenticate() {
-    set_key github.login "Github username" "smithj" "^[0-9a-zA-Z][0-9a-zA-Z-]*$" "See: https://github.com" "$INSTRUCTOR_GITHUB"
-    github_login=$(git config --global github.login)
+    github_configure
+
     if [[ ! -f ~/.token ]]; then
         token="HTTP/1.1 401 Unauthorized"
         code=''
@@ -195,30 +237,42 @@ github_authenticate() {
             echo "Authenticated!"
         else
             printf "Error: "
-            echo $token | grep "HTTP/..."
+            echo "$token" | grep "HTTP/..."
             echo "Sorry, try again later."
             exit 1
         fi
     fi
 }
 
-github_share_key() {
-    echo "Sharing public key..."
-    curl -i -H "Authorization: token $(cat ~/.token)" -d "{\"title\": \"$(hostname)\", \"key\": \"$(cat ~/.ssh/id_rsa.pub)\"}" https://api.github.com/user/keys 2> /dev/null > /dev/null
-}
-
 github_set_name() {
+    github_authenticate
+
     echo "Updating Github profile information..."
     curl --request PATCH -H "Authorization: token $(cat ~/.token)" -d "{\"name\": \"$(git config --global user.name)\"}" https://api.github.com/user 2> /dev/null > /dev/null
 }
 
+# Share the public key
 github_setup_ssh() {
-    generate_key
-    
-    # Force accept host key
+    github_authenticate
+
+    # Check if public key is shared
+    publickey_shared=$(curl -i https://api.github.com/users/$github_login/keys 2> /dev/null)
+    # If not, share it
+    if [[ -z $(echo "$publickey_shared" | grep $(cat ~/.ssh/id_rsa.pub | sed -e 's/ssh-rsa \(.*\)=.*/\1/')) ]]; then
+        echo "Sharing public key..."
+        curl -i -H "Authorization: token $(cat ~/.token)" -d "{\"title\": \"$(hostname)\", \"key\": \"$(cat ~/.ssh/id_rsa.pub)\"}" https://api.github.com/user/keys 2> /dev/null > /dev/null
+    fi
+    # Test SSH connection on default port (22)
     ssh_test=$(ssh -oStrictHostKeyChecking=no git@github.com 2>&1)
     if [[ -z $(echo $ssh_test | grep $github_login) ]]; then
-        github_share_key
+        echo "Your network has blocked port 22; trying port 443..."
+        printf "Host github.com\n  Hostname ssh.github.com\n  Port 443" >> ~/.ssh/config
+        # Test SSH connection on port 443
+        ssh_test=$(ssh -oStrictHostKeyChecking=no git@github.com 2>&1)
+        if [[ -z $(echo $ssh_test | grep $github_login) ]]; then
+            echo "WARNING: Your network has blocked SSH."
+            ssh_works=false
+        fi
     fi
 }
 
@@ -233,6 +287,8 @@ github_request_discount() {
 }
 
 github_create_private_repo() {
+    github_setup_ssh
+    
     result=$(curl -H "Authorization: token $(cat ~/.token)" https://api.github.com/repos/$github_login/$REPO 2> /dev/null)
     if [[ ! -z $(echo $result | grep "Not Found") ]]; then
         echo "Creating private repository $github_login/$REPO on Github..."
@@ -282,14 +338,11 @@ github_check_email() {
 }
 
 github_user() {
-    curl -i https://api.github.com/users/$github_login 2> /dev/null
+    curl -i https://api.github.com/users/$1 2> /dev/null
 }
 
 github_setup() {
-    github_join
-    github_authenticate
     github_set_name
-    github_setup_ssh
     github_create_private_repo
     github_add_collaborator $INSTRUCTOR_GITHUB
     setup_repo
@@ -305,10 +358,10 @@ setup_repo() {
     configure_remotes "$origin" "$upstream"
     file_open $REPO
     cd $REPO
-    git push origin master
+    git push origin master # 2> /dev/null
     result=$(echo $?)
     if [[ $result != 0 ]]; then
-        echo "Unable to push. Your network blocked SSH."
+        echo "Unable to push. Your network blocked SSH (port 22)."
         echo "Failed. Try again when you have full network access."
     else
         file_open "https://github.com/$github_login/$REPO"
@@ -328,18 +381,15 @@ clean() {
     sed -i s/.*github.com.*// ~/.ssh/known_hosts
     git config --global --unset user.name
     git config --global --unset user.email
-    git config --global --unset github.login
+    git config --global --unset login.github
     rm -f ~/.ssh/id_rsa*
     rm -f ~/.token
 }
 
 if [ $# == 0 ]; then
-    configure_git
     github_setup
 elif [[ $1 == "clean" ]]; then
     clean
 elif [[ $1 == "collaborators" ]]; then
     github_add_collaborators
 fi
-
-# github_user
